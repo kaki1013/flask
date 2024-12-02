27bfd01855244e618e91cac1ed3836789e790b67
Myung-Kyu Lee <65400693+kaki1013@users.noreply.github.com>
2024-12-02 21:48:31 +0900

    Update app.py
    delete /test
    update /api/food

--- app.py
+++ app.py
@@ -2,25 +2,28 @@ from flask import Flask, render_template, request, jsonify
 from PIL import Image
 import io
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 import cv2
+import os
+
+import openai
+import base64
+from openai import OpenAI
+import json
+from pydantic import BaseModel
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('./index.html')
-    
-@app.route("/test")
-def test():
-    return render_template('./index.html')
 
-# GET 요청을 처리하는 간단한 엔드포인트
+# test : GET 요청을 처리하는 간단한 엔드포인트
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
-# POST 요청을 처리하는 엔드포인트
+# test : POST 요청을 처리하는 엔드포인트
 @app.route('/api/echo', methods=['POST'])
 def echo():
     data = request.json  # JSON 데이터를 받음
@@ -29,13 +32,13 @@ def echo():
     
     return jsonify({"received": data})
 
-# 간단한 파라미터 처리
+# test : 간단한 파라미터 처리
 @app.route('/api/square/<int:number>', methods=['GET'])
 def square(number):
     result = number ** 2
     return jsonify({"number": number, "square": result})
 
-# 이미지 크기를 반환하는 엔드포인트
+# test : 이미지 크기를 반환하는 엔드포인트
 @app.route('/api/image-size', methods=['POST'])
 def image_size():
     if 'file' not in request.files:
@@ -53,34 +56,78 @@ def image_size():
     except Exception as e:
         return jsonify({"error": f"Invalid image file: {str(e)}"}), 400
 
-def foodModel():
-    model = tf.keras.Sequential([
-               tf.keras.layers.ZeroPadding2D(padding=3, input_shape = (64,64,3)),
-               tf.keras.layers.Conv2D(64, (11,11), strides=1),
-               tf.keras.layers.BatchNormalization(axis = 3),
-               tf.keras.layers.ReLU(),
-               tf.keras.layers.MaxPool2D(),
+# ============================================================================
+# 환경변수에서 API_KEY 읽기
+api_key = os.getenv("API_KEY")
 
-               tf.keras.layers.Conv2D(64, (2,2), strides=1),
-               tf.keras.layers.BatchNormalization(axis = 3),
-               tf.keras.layers.ReLU(),
-               tf.keras.layers.MaxPool2D(),
+if not api_key:
+    raise ValueError("API_KEY environment variable is not set.")
+    
+client = OpenAI(api_key=api_key)
+# ============================================================================
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+# ============================================================================      
+# 음식
+class Food(BaseModel):
+    foodName: str
+    carbohydrates: int
+    protein: int
+    fat: int
+    sodium: int
 
+class Food_Reasponse(BaseModel):
+    resp: list[Food]
 
-               tf.keras.layers.Conv2D(64, (2,2), strides=1),
-               tf.keras.layers.BatchNormalization(axis = 3),
-               tf.keras.layers.ReLU(),
-               tf.keras.layers.MaxPool2D(),
+# 알약
+class Exist(BaseModel):
+    exist: bool
 
-               tf.keras.layers.Flatten(),
-               tf.keras.layers.Dense(1, activation="sigmoid")
-    ])
-    return model
+# 숫자 - 혈당
+class One_Digit_Reasponse(BaseModel):
+    digit: int
 
-food_model = foodModel()
-weights_path = "my_food_model(88_99).h5"
-food_model.load_weights(weights_path)
+# 숫자 - 혈압
+class Two_Digit_Reasponse(BaseModel):
+    sys: int  # 수축 : systolic blood pressure
+    dia: int  # 이완 : diastolic blood pressure
+# ============================================================================
+dict_type = {
+    "food" : ["What is in this image?", Food_Reasponse],
+    "peel" : ["Is there peels in this image?", Exist],
+    "glucose" : ["What is the number in this glucometer image?", One_Digit_Reasponse],
+    "sphygmomanometer" : ["What is the two numbers(sys, dia) in this sphygmomanometer image?", Two_Digit_Reasponse],
+}
+# ============================================================================
+# 이미지 파일을 Vision API에 전달
+def analyze_image(image_path, types):
+  response = client.beta.chat.completions.parse(    
+    model="gpt-4o-mini",
+    messages=[
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": dict_type[types][0],
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url":  f"data:image/jpeg;base64,{encode_image(image_path)}"
+            },
+          },
+        ],
+      }
+    ],
+    response_format=dict_type[types][1],
+  )
 
+  json_data = json.loads(response.choices[0].message.content)
+  return json_data
+# ============================================================================
 # 음식 인식 AI 추론 결과를 반환하는 엔드포인트
 @app.route('/api/food', methods=['POST'])
 def is_food():
@@ -93,15 +140,18 @@ def is_food():
 
     try:
         image = Image.open(io.BytesIO(file.read()))
-        # image = image.resize((64, 64))  # (64, 64, 3)
-        image = image.convert('RGB')  # RGBA -> RGB 변환
-        image_array = np.array(image)
-        image_array = cv2.resize(image_array, (64, 64))  # cv2 test
-        image_array = image_array /255.0
-        image_array = np.expand_dims(image_array, axis=0)  # (1, 64, 64, 3)
-        
-        guess = food_model(image_array)
-        guess = round(float(guess[0][0])) # Food = 0 / Non-Food = 1
-        return jsonify({"guess": guess})
+
+        # 이미지를 임시 저장
+        temp_path = "temp_food_image.png"
+        image.save(temp_path)
+
+        description = analyze_image(temp_path, "food")
+        print(json.dumps(description, indent=4))
+
+        # 임시 파일 삭제 (안전하게 처리)
+        os.remove(temp_path)
+
+        # 결과 반환
+        return jsonify({"guess": description})
     except Exception as e:
         return jsonify({"error": f"Invalid image file: {str(e)}"}), 400
